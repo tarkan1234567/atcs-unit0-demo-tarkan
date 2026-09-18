d5fd196b3e8478576cad81bd21253604899654f8
tarkan1234567 <hee.tarkan@hermits.org>
2026-09-18 18:41:04 +0000

    removed exponents

--- src/calculator.py
+++ src/calculator.py
@@ -20,10 +20,6 @@ while not isinstance(b, numbers.Number):
     except ValueError:
         continue
 
-def power(a, b):
-    """Returns the power of a and b."""
-    return a ** b
-
 def multiply(a, b):
     """Return the product of a and b."""
     return a * b
@@ -51,7 +47,6 @@ def main():
     print(a," - ",b," =", subtract(a, b))
     print(a," * ",b," =", multiply(a, b))
     print(a," / ",b," =", divide(a, b))
-    print(a," ^ ",b," =", power(a, b))
 
 if __name__ == "__main__":
     main()
